5a504e765587f31d884d2c063161fb1dccfec7db
Matheus Saunders <matheussaunders7@gmail.com>
2026-02-27 18:04:20 -0300

    adicionei recompensas para o heroi pos cada batalha

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from personagens import Heroi, Monstro, Orc
 from combate import batalha
 
+def recompensas(heroi): #dando recompensas para o heroi pos turno
+    heroi.cooldown = 0
+    print("Seu cooldown foi resetado!")
+    cura = int(heroi.vida_max * 0.2)
+    antes = heroi.vida
+    heroi.vida = heroi.vida + cura
+    if heroi.vida > heroi.vida_max:
+        heroi.vida = heroi.vida_max
+    cura_real = heroi.vida - antes
+    print(f"Voce recuperou {cura_real}, agora sua vida é {heroi.vida}/{heroi.vida_max}!")
+
 def main():
     goblin = Monstro("goblin", 80, 8)
     orc = Orc("Orc", 120, 12)
@@ -10,9 +21,16 @@ def main():
     p1 = Heroi(nome, 100, 10)
     for inimigo in inimigos: 
         print(f"Um {inimigo.nome} apareceu!!")
-        batalha(p1, inimigo)    
+        batalha(p1, inimigo)
         if not p1.esta_vivo():
             break
+        else: 
+            recompensas(p1)
+            print("-" * 25)
+    if p1.esta_vivo():
+        print("VOCE VENCEU TODAS AS FASES!!")
+    else:
+        print("FIM DE JOGO!")
 
 if __name__ == "__main__":
     main()
